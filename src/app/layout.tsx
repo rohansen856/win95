371c1edf3eb7decb4a26bec173071ad15ff9d57b
rcsen <rohansen856@gmail.com>
2024-08-29 00:24:17 +0530

    library added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import '@react95/icons/icons.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
