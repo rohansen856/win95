4f7e130f27b3273212bf44708180a08aebb72e7e
rcsen <rohansen856@gmail.com>
2024-08-29 00:30:41 +0530

    add css added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import '@react95/core/GlobalStyle';
+import '@react95/core/themes/win95.css';
 import '@react95/icons/icons.css';
 
 const inter = Inter({ subsets: ["latin"] });
